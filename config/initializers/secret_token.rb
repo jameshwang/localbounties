27e8cf3502191a8db1363846d4d81fd0ea4652be
Vincent Samaco <vsamaco@gmail.com>
2013-03-02 15:53:43 -0800

    first commit init files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Localbounties::Application.config.secret_token = 'aa2c7ad8e59a2903e52157d941cb7accad8016e2316a5af7a7ec842e55191354cfaa4c1abb74b9c55fc6b238fa4f50a26033184134780502d9209c2618c48972'
