1ef64eed459b598a7d144166128a6a662560b37f
Vincent Samaco <vsamaco@gmail.com>
2013-03-03 22:52:41 -0800

    Added updated db schema

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  layout 'main_sublayout'
+  layout :layout
+
+  private
+
+  def layout
+    # or turn layout off for every devise controller:
+    devise_controller? ? "sessions" : "main_sublayout"
+  end
 
 end
