177cd00402c770d1a755a4809b11d589187d78e1
Connor Warnock <connorwarnock@gmail.com>
2013-03-04 10:07:21 -0800

    Removes assets after deploy

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
 echo 'Compiling assets...'
+echo '==================='
 rake assets:precompile
 
 echo 'Pushing to heroku...'
+echo '==================='
 git add .
 git add -u
 git commit -m 'Compiles assets'
 git push heroku master
 
 echo 'Removing assets...'
+echo '==================='
 rm -rf 'public/assets'
 
 echo 'Pushing to github...'
+echo '==================='
 git add .
 git add -u
 git commit -m 'Removes assets after deploy'
 git push origin master
+
+echo 'Success!'
+echo '==================='
